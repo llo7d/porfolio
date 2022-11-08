e9395742b7d908cf49db43489d82dcd51b2b17f5
peterlloyd1 <63150386+peterlloydskubur@users.noreply.github.com>
2022-11-08 15:28:24 +0100

    added dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+
+  ],
+  plugins: [
+    require("flowbite/plugin")
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
 }
